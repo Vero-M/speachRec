a00c0100860622c427faa284589f7dfcd6d1595b
19013593 <19013593@vcconnect.co.za>
2021-05-04 09:26:39 +0200

    Speech method added

--- app/src/main/java/com/example/speachrec/MainActivity.java
+++ app/src/main/java/com/example/speachrec/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.speachrec;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,10 @@ import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +27,29 @@ public class MainActivity extends AppCompatActivity {
     public void speechConvert(View view) {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
+        if (intent.resolveActivity(getPackageManager()) !=null)
+        {
+            startActivityForResult(intent,10);
+        }else 
+        {
+            Toast.makeText(this, "Device doesn't support speech", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //Set the speech to textview
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode)
+        {
+            case 10:
+                if(resultCode == RESULT_OK && data != null)
+                {
+                    //pass it onto the textview
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    textView.setText(result.get(0));
+                }break;
+        }
     }
 }
