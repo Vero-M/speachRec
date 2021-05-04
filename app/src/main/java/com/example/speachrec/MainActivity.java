8937e586eace5bd40179b935fc50e11f9df024b6
19013593 <19013593@vcconnect.co.za>
2021-05-04 09:15:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/speachrec/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.speachrec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView2);
+    }
+    //method for speech recognition
+    public void speechConvert(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+    }
+}
